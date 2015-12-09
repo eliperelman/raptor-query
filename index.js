0b49f3e590cc6bd0a123cc227b623b9d15fcc7de
Eli Perelman <eli@eliperelman.com>
2015-12-09 09:57:41 -0500

    Integrate raptor/query into new cli tool

--- index.js
+++ index.js
@@ -1,87 +1,115 @@
-var R = require('ramda');
-var database = require('./database');
+'use strict';
 
-var queries = {
-  measure: (query) => {
-    return `SELECT MEAN(value) as value
-      FROM "measure"
-      WHERE "metric" = '${query.metric}'
-      AND "context" = '${query.context}'
-      AND "branch" = '${query.branch}'
-      AND "device" = '${query.device}'
-      AND "memory" = '${query.memory}'
-      AND "test" = '${query.test}'
-      AND ${query.timeFilter}
-      GROUP BY * fill(none)`;
-  },
-  memory: (query) => {
-    return `SELECT MEAN(value) as value
-      FROM "memory"
-      WHERE "metric" = '${query.metric}'
-      AND "context" = '${query.context}'
-      AND "branch" = '${query.branch}'
-      AND "device" = '${query.device}'
-      AND "memory" = '${query.memory}'
-      AND "test" = '${query.test}'
-      AND ${query.timeFilter}
-      GROUP BY * fill(none)`;
-  },
-  mtbf: (query) => {
-    return `SELECT SUM(value) / SUM(failures) as value
-      FROM "mtbf"
-      WHERE "node" = '${query.node}'
-      AND "branch" = '${query.branch}'
-      AND "device" = '${query.device}'
-      AND "memory" = '${query.memory}'
-      AND ${query.timeFilter}
-      GROUP BY time(1s) fill(none)`;
-  },
-  power: (query) => {
-    return `SELECT value
-      FROM "power"
-      WHERE "test" = '${query.test}'
-      AND "context" = '${query.context}'
-      AND "branch" = '${query.branch}'
-      AND "device" = '${query.device}'
-      AND "memory" = '${query.memory}'
-      AND ${query.timeFilter}`;
-  }
-};
+let R = require('ramda');
+let query = require('./lib');
 
 /**
- * Remove extra whitespace and newlines from a query
- * @param {String} queryString
- * @returns {String}
+ * Determine if an object is a `measure` measurement
+ * @param {Object} options
+ * @returns {Boolean}
  */
-var clean = R.pipe(R.replace(/\n[ ]+/g, ' '), R.trim);
+let isMeasure = R.pipe(R.prop('measurement'), R.equals('measure'));
 
 /**
- * Fetch the query string for a given measurement using options to fill in query values
- * @param {String} measurement InfluxDB measurement
- * @param {Object} options query values
+ * Determine if an object is a `memory` measurement
+ * @param {Object} options
+ * @returns {Boolean}
  */
-var getRawQuery = (measurement, options) => R.call(R.prop(measurement, queries), options);
+let isMemory = R.pipe(R.prop('measurement'), R.equals('memory'));
 
 /**
- * Fetch the query string for a given set of options
+ * Determine if an object is a `measure` or `memory` measurement
+ * @param {Object} options
+ * @returns {Boolean}
+ */
+let isMeasureOrMemory = R.converge(R.or, [isMeasure, isMemory]);
+
+/**
+ * Determine if an object's `context` property is empty
+ * @param {Object} options
+ * @returns {Boolean}
+ */
+let noContext = R.pipe(R.prop('context'), R.converge(R.or, [R.isNil, R.isEmpty]));
+
+/**
+ * Determine if an object's `metric` property is empty
+ * @param {Object} options
+ * @returns {Boolean}
+ */
+let noMetric = R.pipe(R.prop('metric'), R.converge(R.or, [R.isNil, R.isEmpty]));
+
+/**
+ * Determine if an object is missing a required context
+ * @param {Object} options
+ * @returns {Boolean}
+ */
+let missingContext = R.converge(R.and, [isMeasureOrMemory, noContext]);
+
+/**
+ * Determine if an object is missing a required metric
+ * @param {Object} options
+ * @returns {Boolean}
+ */
+let missingMetric = R.converge(R.and, [isMeasureOrMemory, noMetric]);
+
+/**
+ * Return the time filter of an object with a time property
  * @param {Object} options
  * @returns {String}
  */
-var getQuery = R.converge(R.pipe(getRawQuery, clean), [R.prop('measurement'), R.identity]);
+let getTimeFilter = R.pipe(
+  R.prop('time'),
+  R.cond([
+    [R.contains(' '), R.identity],
+    [R.T, R.concat('time > now() - ')]
+  ])
+);
 
 /**
- * Execute a query against an InfluxDB database
- * 1. Create a database client
- * 2. Fetch the query string
- * 3. Execute the query against the database
- * @param options
- * @returns {Promise}
+ * Convert object with time property to object with timeFilter
+ * @param {Object} options
+ * @returns {Object}
  */
-module.exports = (options) => {
-  return Promise
-    .all([
-      database(options),
-      getQuery(options)
-    ])
-    .then(R.apply((client, query) => client.query(query)));
+let formatTime = R.converge(R.merge, [
+  R.pipe(getTimeFilter, R.objOf('timeFilter')),
+  R.omit('time')
+]);
+
+module.exports = (cli) => {
+  let command = cli.command('query <measurement>');
+
+  command
+    .description('Run a query against an InfluxDB data source')
+    .option('--context [origin]', 'Filter records to a particular application context; required for measure and memory measurements')
+    .option('--metric [name]', 'Filter records to a particular metric; required for measure and memory measurements')
+    .option('--branch [name]', 'Filter records to those run against a particular branch', 'master')
+    .option('--device [identifier]', 'Filter records to those run against a particular device type')
+    .option('--memory [memoryMB]', 'Filter records to those run against a particular amount of memory for the targeted device', 512)
+    .option('--test [name]', 'Filter records to those run with a particular test')
+    .option('--node [name]', 'Filter records to those run on a particular node')
+    .option('--time [expression]', 'Filter records to a particular InfluxDB time query')
+    .action(function(args) {
+      /**
+       * Command flow:
+       * 1. Ensure --context for measure and memory queries
+       * 2. Ensure --metric for measure and memory queries
+       * 3. Convert --time to timeFormat
+       * 4. Pass options to execute to run query
+       * @param {Object} options
+       * @returns {Promise}
+       */
+      return Promise
+        .resolve(R.merge({ measurement: args.measurement }, cli.getOptions(this)))
+        .then(R.cond([
+          [missingContext, cli.errors('--context is required for measure or memory queries')],
+          [missingMetric, cli.errors('--metric is required for measure or memory queries')],
+          [R.T, R.pipe(formatTime, query)]
+        ]))
+        .then(cli.JSON)
+        .catch(cli.exits)
+    });
+
+  cli.usesDatabase(command);
+
+  return command;
 };
